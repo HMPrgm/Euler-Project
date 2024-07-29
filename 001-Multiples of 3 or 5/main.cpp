5e968c674dd99aff94dd05c301bf79005757464c
MSI Laptop <henrymunprof@gmail.com>
2024-07-29 17:57:53 -0400

    First 2 problems

--- 001-Multiples of 3 or 5/main.cpp
+++ 001-Multiples of 3 or 5/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+// PROBLEM 1
+// Find the sum of all the multiples of 3 or 5 below 1000
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    int sum = 0;
+    //collect all multiples of 3
+    for (int i = 0; i < 1000; i+=3) {
+        
+        sum += i;
+    }
+    // collect all multiples of 5
+    for (int i=0;i<1000; i+=5) {
+        if (i % 3 == 0) {
+            continue;
+        }
+        sum += i;
+    }
+    cout << "The sum of all multiples of 3 or 5 below 1000: " << sum;
     return 0;
 }
---
+++ 002-Even Number Fibonacci/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// PROBLEM 2
+// Find the sum of the even-valued Fibonnaci terms < four million.
+using namespace std;
+
+long sumEvenFibonnaci(int max);
+
+int main()
+{
+    long answer = sumEvenFibonnaci(4000000);
+    cout << "um of the even-valued Fibonnaci terms < four million: " << answer;
+    return 0;
+}
+
+long sumEvenFibonnaci(int max) {
+    long sum = 0;
+    int  f0 = 0;
+    int f1 = 1;
+    while (f1 < max) {
+        sum += f1 %2 == 0? f1:0;
+        int temp = f1;
+        f1 += f0;
+        f0 = temp;
+    } 
+    return sum;
+}
